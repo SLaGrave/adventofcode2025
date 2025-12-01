e79a1dacbc75348f100263552cb4a093b715fc93
Sam LaGrave <srlagrave@gmail.com>
2025-12-01 10:55:10 -0600

    Cleanup/generalize

--- python_impl/day01.py
+++ python_impl/day01.py
@@ -1,32 +1,35 @@
 
-def load_data():
-    with open("../inputs/day01.txt", "r") as f:
+def load_data(data_location = "../inputs/day01.txt"):
+    """Loads data from a txt file.
+    
+    Read lines, strip each one, and return as a list
+    """
+    with open(data_location, "r") as f:
         data = [q.strip() for q in f.readlines()]
     return data
 
 
 def part_one(data):
-    dial = 50
     counter = 0
+    dial = 50
     for turn in data:
-        if turn[0] == "R": turn = turn[1:]
-        else: turn = f"-{turn[1:]}"
-        turn = int(turn)
+        # Transform into integer
+        # "L50" -> -50
+        # "R109" -> 109
+        turn = int(turn[1:]) if turn[0] == "R" else int(f"-{turn[1:]}")
         dial = (dial + turn) % 100
         if dial == 0: counter += 1
     return counter
 
 
 def part_two(data):
-    dial = 50
     counter = 0
+    dial = 50
     for turn in data:
-        if turn[0] == "L": sign = -1
-        else: sign = 1
+        sign = 1 if turn[0] == "R" else -1
         turn = int(turn[1:])
         for _ in range(turn):
-            dial += sign*1
-            dial %= 100
+            dial = (dial + sign) % 100
             if dial == 0: counter += 1
     return counter
 
